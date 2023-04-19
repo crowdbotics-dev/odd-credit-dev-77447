3a3eca7800fd66b693b490fcd46f416d9ccb13b8
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 00:19:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-credit-dev-77447",
+  url: "https://odd-credit-dev-77447.botics.co",
+  api: "https://odd-credit-dev-77447.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
